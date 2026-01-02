ca9d295ade0ea18d7c9e71691a67309a8efcea6c
molach <moshe.lach@yahoo.com>
2026-01-02 09:31:10 -0500

    demo

--- src/A_catchup/HelloUser.java
+++ src/A_catchup/HelloUser.java
@@ -1,6 +1,8 @@
 // Prompts the user for their name, reads it in, and prints a greeting.
 // Uses new Java 25 features. To learn more: https://openjdk.org/jeps/512
 
+// whatever
+
 void main() {
     String name = IO.readln("What is your name? ");
     printGreeting(name);
